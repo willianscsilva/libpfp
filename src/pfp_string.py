46d415d0d569a3ace99ae5e93a0e3a5de799f054
will <willianscsilva@gmail.com>
2013-04-12 00:57:50 -0200

    string functions

--- src/pfp_string.py
+++ src/pfp_string.py
@@ -1,11 +1,71 @@
+import htmlentitydefs
+import re,cgi,sys
 class pfp_string:
 	def addslashes(self,str):
 		return str.replace("'","\\\'").replace("\"","\\\"")
 
 	def explode(self,delimiter,str,limit=0):
-		if limit == 0:
-			return str.split(delimiter)
-		elif type(limit) == int:
-			return str.split(delimiter,limit)
-		else:
-			print "ParameterError: The limit parameter must be a integer" 
+		try:
+			if limit == 0:
+				return str.split(delimiter)
+			elif type(limit) == int:
+				return str.split(delimiter,limit)
+			else:
+				print "Incorrect argument, on method explode(",delimiter,", ",str,", ",limit,"), expected integer" 
+		except TypeError, te:
+			print "Incorrect argument, on method explode(",delimiter,", ",str,", ",limit,"), expected a character buffer object"
+		except AttributeError, ae:
+			print "Incorrect argument, on method explode(",delimiter,", ",str,", ",limit,"), expected string"
+		except:
+			print "Unexpected error:", sys.exc_info()[0]
+			raise
+
+	def html_entities(self,str):
+		return cgi.escape(str)
+
+	def html_entity_decode_char(self,m, defs=htmlentitydefs.entitydefs):
+		try:
+			return defs[m.group(1)]
+		except KeyError:
+			return m.group(0)
+
+	def html_entity_decode(self,str):
+		try:
+			pattern = re.compile("&(\w+?);")
+			return pattern.sub(self.html_entity_decode_char, str)
+		except AttributeError, ae:
+			print "Incorrect argument, on method html_entity_decode(",str,"), expected string"
+		except TypeError, te:
+			print "Incorrect argument, on method html_entity_decode(",str,"), expected string or buffer"
+		except:
+			print "Unexpected error:", sys.exc_info()[0]
+			raise
+	
+	def implode(self,delimiter,array):
+		try:
+			return delimiter.join(array)
+		except AttributeError, ae:
+			print "Incorrect argument, on method implode(",delimiter,", ",array,"), expected string delimiter"
+		except TypeError, te:
+			print "Incorrect argument, on method implode(",delimiter,", ",array,"), expected list (array)"
+		except:
+			print "Unexpected error:", sys.exc_info()[0]
+			raise
+
+	def lcfirst(self,str):
+		try:
+			return str.replace(str[0],str[0].lower())
+		except AttributeError, ae:
+			print "Incorrect argument, on method lcfirst(",str,"), expected string"
+		except:
+			print "Unexpected error:", sys.exc_info()[0]
+			raise
+
+	def ucfirst(self,str):
+		try:
+			return str.replace(str[0],str[0].upper())
+		except AttributeError, ae:
+			print "Incorrect argument, on method ucfirst(",str,"), expected string"
+		except:
+			print "Unexpected error:", sys.exc_info()[0]
+			raise
--- test_pfp_string.py
+++ test_pfp_string.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
 import sys
 from src.pfp_string import pfp_string
 
@@ -10,7 +12,18 @@ if __name__ == "__main__":
 		if argm == "addslashes":
 			print pfp.addslashes("O'CONNEL")
 		elif argm == "explode":
-			print pfp.explode("-","1982-09-09")
+			print pfp.explode("-","2010-20-03")
+		elif argm == "html_entities":
+			print pfp.html_entities('<b>á</b>')
+		elif argm == "html_entity_decode":
+			print pfp.html_entity_decode('&lt;b&gt;á&lt;/b&gt;')
+		elif argm == "implode":
+			array = pfp.explode("-",'20-03-2010')
+			print pfp.implode("-",array)
+		elif argm == "lcfirst":
+			print pfp.lcfirst("HELLO WORLD")
+		elif argm == "ucfirst":
+			print pfp.ucfirst(1)
 	else:
 		program_name = sys.argv[0]
 		print "\nIncorrect arguments, see how to use:\n"
